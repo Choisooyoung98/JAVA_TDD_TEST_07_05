2697aa4c3049573a2228bef60045f1f692758678
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-05 16:53:33 +0900

    곱하기 까지 구현

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -1,7 +1,20 @@
 package com.ll.exam;
 
 public class Calculator {
-    public static int add(String s) {
+    public static int calc(String s) {
+        boolean isMulti = s.indexOf(" * ") != -1;
+        boolean isSubtract = s.indexOf(" - ") != -1;
+        boolean isPlus = s.indexOf(" + ") != -1;
+        if (isPlus) {
+            return plus(s);
+        } else if (isMulti) {
+            return multi(s);
+        } else if (isSubtract) {
+            return subtract(s);
+        }
+        return 0;
+    }
+    public static int plus(String s) {
         String[] sBits = s.split(" \\+ ");
         int no1, no2;
         no1 = Integer.parseInt(sBits[0]);
@@ -10,11 +23,19 @@ public class Calculator {
         return no1 + no2;
     }
 
-    public static int minus(String s) {
+    public static int subtract(String s) {
         String[] sBits = s.split(" \\- ");
         int no1, no2;
         no1 = Integer.parseInt(sBits[0]);
         no2 = Integer.parseInt(sBits[1]);
         return no1 - no2;
+    }
+
+    public static int multi(String s) {
+        String[] sBits = s.split(" \\* ");
+        int no1, no2;
+        no1 = Integer.parseInt(sBits[0]);
+        no2 = Integer.parseInt(sBits[1]);
+        return no1 * no2;
     }
 }
--- src/test/java/com/ll/exam/exam.java
+++ src/test/java/com/ll/exam/exam.java
@@ -6,20 +6,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class exam {
     @Test
     public void 더하기() {
-        int rs = Calculator.add("10 + 10");
+        int rs = Calculator.calc("10 + 10");
 
         assertEquals(20, rs);
     }
     @Test
     public void 빼기() {
-        int rs = Calculator.minus("10 - 5");
+        int rs = Calculator.calc("10 - 5");
 
         assertEquals(5, rs);
     }
 
     @Test
     public void 곱하기() {
-        int rs = 10 * 5;
+        int rs = Calculator.calc("10 * 5");
 
         assertEquals(50, rs);
     }
